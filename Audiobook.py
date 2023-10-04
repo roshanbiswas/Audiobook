1d6c38751ce3abfa5b6a8e2dedff3222e1c6e30d
roshanbiswas <roshanbiswas10@gmail.com>
2023-10-04 20:12:24 +0530

    updated audiobook

--- Audiobook.py
+++ Audiobook.py
@@ -1,17 +1,57 @@
 # imports
 import PyPDF2
 import pyttsx3
+import os
 import time
 
-path = open('script.pdf', 'rb')
-pdfReader = PyPDF2.PdfReader(path)
+# Function to extract text from a PDF file
+def extract_from_pdf(file_path, start_page=0):
+    with open(file_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page_num in range(start_page, len(pdf_reader.pages)):
+            text += pdf_reader.pages[page_num].extract_text()
+    return text
 
-# the page with which you want to start
-from_page = pdfReader.pages[1]
+# Function to extract text from a TXT file
+def extract_from_txt(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
 
-text = from_page.extract_text()
-rate = 300
-speak = pyttsx3.init()
-speak.setProperty('rate', rate)
-speak.say(text)
-speak.runAndWait()
+# Main function to detect file type and extract text accordingly
+def extract_text(file_path, start_page=0):
+    _, file_extension = os.path.splitext(file_path)
+    if file_extension == ".pdf":
+        return extract_from_pdf(file_path, start_page)
+    elif file_extension == ".txt":
+        return extract_from_txt(file_path)
+    else:
+        return "Unsupported file type."
+
+def convert_text_to_speech(text, rate=350):
+    # Initialize the speech engine
+    speak = pyttsx3.init()
+    speak.setProperty('rate', rate)
+    speak.say(text)
+    speak.runAndWait()
+
+def user_interaction():
+    # Get file path from user
+    file_path = input("Please enter the path to your file: ")
+
+    # Detect file type
+    _, file_extension = os.path.splitext(file_path)
+
+    # If PDF, ask for a starting page
+    start_page = 0
+    if file_extension == ".pdf":
+        try:
+            start_page = int(input("Enter the page number to start reading from (default is 0): "))
+        except ValueError:
+            print("Invalid input. Starting from the first page.")
+    
+    # Extract and Convert text
+    text = extract_text(file_path, start_page)
+    convert_text_to_speech(text)
+    
+user_interaction()
