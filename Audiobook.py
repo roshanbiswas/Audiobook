83c9a5d8d41c48054fe022add56a7018cd56cc5d
roshanbiswas <roshanbiswas10@gmail.com>
2023-11-06 22:08:51 +0530

    Added doctrings for each function.

--- Audiobook.py
+++ Audiobook.py
@@ -3,9 +3,16 @@ import PyPDF2
 import pyttsx3
 import os
 import time
-
-# Function to extract text from a PDF file
+ 
 def extract_from_pdf(file_path : str):
+    """ Function to extract text from a PDF file
+
+    Args:
+        file_path (str): path to file containing text.
+
+    Returns:
+        txt: text from audiobook file
+    """
     with open(file_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         text = ""
@@ -13,13 +20,27 @@ def extract_from_pdf(file_path : str):
             text += pdf_reader.pages[page].extract_text()
     return text
 
-# Function to extract text from a TXT file
 def extract_from_txt(file_path: str):
+    """ Function to extract text from a TXT file
+
+    Args:
+        file_path (str): path to file containing text.
+
+    Returns:
+        txt: text from audiobook file
+    """
     with open(file_path, 'r') as file:
         return file.read()
 
-# function to detect file type and extract text accordingly
 def extract_text(file_path: str):
+    """ function to detect file type and extract text accordingly
+
+    Args:
+        file_path (str): path to file containing text.
+
+    Returns:
+        txt: text from audiobook file 
+    """
     _, file_extension = os.path.splitext(file_path)
     if file_extension == ".pdf":
         return extract_from_pdf(file_path)
@@ -29,6 +50,12 @@ def extract_text(file_path: str):
         return "Unsupported file type."
 
 def convert_text_to_speech(text : str, rate : int = 250):
+    """ function that uses pyttsx3 to convert text to speech
+
+    Args:
+        text (str): text which is converted to speech
+        rate (int, optional): rate of speech. Defaults to 250.
+    """
     # Initialize the speech engine
     speak = pyttsx3.init()
     speak.setProperty('rate', rate)
@@ -36,6 +63,8 @@ def convert_text_to_speech(text : str, rate : int = 250):
     speak.runAndWait()
 
 def user_interaction():
+    """takes all needed inputs and calls extract_text() and convert_text_to_speech()
+    """
     # Get file path from user
     file_path = input("Please enter the path to your file: ")
 
